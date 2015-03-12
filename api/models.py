243041ae7500105a3bc5efba79e3c2ba90614b9f
Albert Wohletz <albert@wohletz.net>
2015-03-11 22:35:59 -0700

    basic model and statics

--- api/models.py
+++ api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class event(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = User()
+    name = models.CharField(max_length=100)
+    text = models.CharField(max_length=5000)
+    hidden = models.BooleanField(default=False)
+
+class subevent(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = User()
+    event = event()
+    name = models.CharField(max_length=100)
+    text = models.CharField(max_length=5000)
+    hidden = models.BooleanField(default=False)
+
+class npc(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = User()
+    name = models.CharField(max_length=100)
+    text = models.CharField(max_length=5000)
+    hidden = models.BooleanField(default=False)
+
+class pc(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = User()
+    name = models.CharField(max_length=100)
+    text = models.CharField(max_length=5000)
+    hidden = models.BooleanField(default=False)
